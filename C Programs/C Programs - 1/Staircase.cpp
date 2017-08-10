6bc5a3df685d2d2164b8fef1a3caf9ed0d06a393
Saikat Ghosh <ghosh_saikat4000@yahoo.co.in>
2017-08-10 08:31:10 +0530

    Add files via upload

---
+++ C Programs/C Programs - 1/Staircase.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+typedef vector <long long> vll;
+
+int main()
+{
+    int no_of_bricks;
+    scanf("%d", &no_of_bricks);
+
+    //f(i, j) represents the number of staircases with i bricks with the height of the largest stair at most j.
+    vector <vector <long long> > no_of_ways(no_of_bricks + 1, vector <long long> (no_of_bricks + 1, 0) );
+
+
+    no_of_ways[0][0] = 1;
+
+    for(int last_height = 1; last_height <= no_of_bricks; last_height++)
+    	no_of_ways[0][last_height] = no_of_ways[1][last_height] = 1;
+
+    for(int bricks = 2; bricks <= no_of_bricks; bricks++)
+    {
+        for(int last_height = 1; last_height <= no_of_bricks - 1; last_height++)
+        {
+            no_of_ways[bricks][last_height] = no_of_ways[bricks][last_height - 1];
+
+            if(bricks >= last_height)
+            {
+                no_of_ways[bricks][last_height] += no_of_ways[bricks - last_height][last_height - 1];
+                //printf("f(%d, %d) = %I64d + \t", bricks, last_height, no_of_ways[bricks][last_height]);
+                //printf("f(%d, %d) = %I64d\n", bricks - last_height, last_height - 1, no_of_ways[bricks - last_height][last_height - 1]);
+            }
+            //printf("f(%d, %d) = %I64d\n", bricks, last_height, no_of_ways[bricks][last_height]);
+        }
+    }
+
+    printf("%I64d\n", no_of_ways[no_of_bricks][no_of_bricks - 1]);
+    return 0;
+}
+
