8b67f4eed6d3a218761e56d5cb1e2541ab87d065
Saikat Ghosh <ghosh_saikat4000@yahoo.co.in>
2017-10-04 17:31:57 +0530

    Add files via upload

---
+++ C Programs/C Programs - 1/Two Gangsters.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main()
+{
+    int harry_cans, larry_cans;
+    scanf("%d %d", &harry_cans, &larry_cans);
+
+    printf("%d %d\n", larry_cans - 1, harry_cans - 1);
+    return 0;
+}
